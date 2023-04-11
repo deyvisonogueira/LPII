b4b46e7cb062ba09c3503d19eccf6bc9693d701c
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2023-04-11 00:20:09 -0300

    Delete polimorfismo directory

--- polimorfismo/src/Henranca/ContaCorrente.java
+++
@@ -1,8 +0,0 @@
-package Herança;
-
-public class ContaCorrente extends Conta {
-  public void atualiza(double taxa) {
-    super.deposita(this.getSaldo() * taxa * 2);
-    super.deposita(-1);
-  }
-}
--- polimorfismo/src/Henranca/ContaPoupanca.java
+++
@@ -1,7 +0,0 @@
-package Herança;
-
-public class ContaPoupanca extends Conta {
-  public void atualiza(double taxa) {
-    super.deposita(this.getSaldo() * taxa * 3);
-  }
-}
